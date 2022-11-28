4551e0f88028ca8616e3372a716266081cacd604
Antonio <antoniovictorbordignon@outlook.com>
2022-11-28 11:50:56 -0300

    website to present solution

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,14 +1,16 @@
-function palindrome(str) {
+function palindrome() {
 
-let removingSpecialCharacters = str.replace(/[^a-z0-9]/gi, "").toLowerCase()
+let word = document.getElementById('word')
+let answer = document.getElementById('answer')
+let w = String(word.value);
+
+let removingSpecialCharacters = w.replace(/[^a-z0-9]/gi, "").toLowerCase()
 let wordBackwards = removingSpecialCharacters.split("").reverse().join("");
 
 if(removingSpecialCharacters == wordBackwards) {
-  return true
+  return answer.innerHTML = `the word ${w} is a palindrome`
 } else {
-  return false
+  return answer.innerHTML = `the word ${w} is not a palindrome`
 }
 
 }
-
-palindrome("A man, a plan, a canal. Panama");
