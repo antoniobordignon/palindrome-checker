f014152c81c46949d9f881f8ee490461b47e82a0
Antonio <antoniovictorbordignon@outlook.com>
2022-11-28 11:56:54 -0300

    empty input validation

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,16 +1,19 @@
 function palindrome() {
 
-let word = document.getElementById('word')
-let answer = document.getElementById('answer')
-let w = String(word.value);
+  let word = document.getElementById('word')
+  let answer = document.getElementById('answer')
+  let w = String(word.value);
 
-let removingSpecialCharacters = w.replace(/[^a-z0-9]/gi, "").toLowerCase()
-let wordBackwards = removingSpecialCharacters.split("").reverse().join("");
+  if (w == "") {
+    return answer.innerHTML = `Please enter a word.`
+  } else {
+    let removingSpecialCharacters = w.replace(/[^a-z0-9]/gi, "").toLowerCase()
+    let wordBackwards = removingSpecialCharacters.split("").reverse().join("");
 
-if(removingSpecialCharacters == wordBackwards) {
-  return answer.innerHTML = `the word ${w} is a palindrome`
-} else {
-  return answer.innerHTML = `the word ${w} is not a palindrome`
-}
-
+    if(removingSpecialCharacters == wordBackwards) {
+      return answer.innerHTML = `the word ${w} is a palindrome.`
+    } else {
+      return answer.innerHTML = `the word ${w} is not a palindrome.`
+    }
+  }
 }
